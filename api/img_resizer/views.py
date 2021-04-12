9c94c00fd42ae2911508e07ad6c79f357d0129ed
trishantpahwa <trishantpahwa@gmail.com>
2021-04-12 09:17:00 +0530

    Added imports for redis and rq.

--- api/img_resizer/views.py
+++ api/img_resizer/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import redis
+from rq import Queue
+from tasks import create_image_set
+
+r = redis.Redis()
+q = Queue(connection=r)
 
 # Create your views here.
 def index(request):
+    image_dir = 'C:\\Users\\Admin\\Desktop'
+    filename = '1545117208706.jpg'
+    q.enqueue(create_image_set, image_dir, filename)
+
+    print("Image resizing", "success")
+
     return HttpResponse('test')
