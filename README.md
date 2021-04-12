bad9776889b9d3ff8eb4eab1d2a9ab46bc390ff7
trishantpahwa <trishantpahwa@gmail.com>
2021-04-12 07:50:04 +0530

    Reset project.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# django-users-demo
+# django-demo
 A demo to get familiar with the djangorestframework.
--- users/users_/users_/users_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .users_app import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user_app/', include('user_app.urls'))
-]
